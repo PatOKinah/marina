3e322b61c79c5b33c540c7adf515ecfcb71190c6
Adam Kałasz <adamkalasz@example.com>
2018-03-19 10:20:24 +0100

    dodana nowa funkcja list_of_yacht

--- marina_main.py
+++ marina_main.py
@@ -1,7 +1,9 @@
+import time
 global PositionList
+global Dict_of_yacht
 
 PositionList = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20]
-
+Dict_of_yacht = {}
 
 def main_menu():
     print("1. Lista stanowisk")
@@ -17,6 +19,7 @@ def main_menu():
             position_menu(PositionList)
         elif wybor == "2":
             print("Lista jachtów")
+            list_of_yacht(Dict_of_yacht)
         elif wybor == "3":
             Loop = False
             exit()
@@ -58,6 +61,24 @@ def release_position(PositionList):
     x = int(input("Wybierz miejsce postojowe: "))
     PositionList[x - 1] = str(x)
     print(PositionList)
+
+def list_of_yacht(Dict_of_yacht):
+    x = int(input("Podaj miejsce postojowe: "))
+    if PositionList[x-1] == x :
+        owner = input("Podaj imię i nazwisko: ")
+        saillicense = input("Podaj numer patentu żeglarskiego: ")
+        degree = input("Podaj stopień żeglarski: ")
+        contact_with_owner = input("Podaj numer telefonu: ")
+        type_of_yacht = input("Typ/Klasa jachtu: ")
+        plate = input("Numer rejestracyjny jachu")
+        last_update = time.strftime("%Y.%m.%d//%H:%M:%S")
+        Dict_of_yacht = {x:{"właściciel": owner, "patent": saillicense, "stopień": degree,
+                            "telefon": contact_with_owner, "typ/klasa": type_of_yacht, "rejestracja": plate,
+                            "modyfikacja": last_update}}
+        print(Dict_of_yacht)
+    else: print("Miejsce postojowe jest już zarezerwowane")
+    return Dict_of_yacht
+
 
 
 print("Marina")
